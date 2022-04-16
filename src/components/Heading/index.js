334ad3a73cda5c7eccb663690d5c079072715293
Aleksey Sokolov <lionsbet@outlook.com>
2022-04-17 01:36:47 +0300

    выполнено домашнее задание

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -2,23 +2,35 @@ import { createElement } from 'react';
 import cn from 'classnames';
 import propTypes from 'prop-types';
 
-import s from './Heading.module.css';
+import s from './Heading.module.scss';
 
-const Heading = ({children, level, className}) => {
+const Heading = ({
+                     children,
+                     level,
+                     className,
+                     backLine
+}) => {
         const el = `h${level}`;
 
         return createElement(el, {
-            className: cn(s.root, className, s[`level${level}`])
+            className: cn(
+                s.root,
+                className,
+                s[`level${level}`],
+                {[s.backLine]: backLine}
+                )
         }, children);
 };
 
 Heading.defaultProps = {
     level: 1,
+    backLine: false,
 }
 Heading.propTypes = {
     level: propTypes.number,
     className: propTypes.string,
-    children: propTypes.node
+    children: propTypes.node,
+    backLine: propTypes.bool,
 }
 
 export default Heading;
--- src/components/config.js
+++ src/components/config.js
@@ -4,5 +4,6 @@ import Footer from "./Footer";
 import Container from "./Container";
 import Heading from "./Heading";
 import Text from "./Text";
+import CharachterCard from "./CharachterCard";
 
-export { Header, Slider, Footer, Container, Heading, Text };
+export { Header, Slider, Footer, Container, Heading, Text, CharachterCard };
